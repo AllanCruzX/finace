45753f6fe20cf4ce845d847463efc7ff5c94abd4
Allan da Cruz Rosa <allan.rosa@ses.ba>
2020-12-09 16:56:05 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NavegacaoModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/conta/conta.app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'conta-app-root',
+  template: '<router-outlet></router-outlet>'
+})
+export class ContaAppComponent {
+  //Componente principal do modulo conta (para gerenciar as rotas)
+
+ }
